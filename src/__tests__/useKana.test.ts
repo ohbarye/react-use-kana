51bc6d6f0ca0dc30581bdb13fa9584c5410bf540
ohbarye <over.rye@gmail.com>
2019-10-12 22:34:26 +0900

    Use own kana extraction logic

--- src/__tests__/useKana.test.ts
+++ src/__tests__/useKana.test.ts
@@ -1,28 +1,149 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useKana } from '../useKana';
 
-test('returns kana based on user input', () => {
-  const { result } = renderHook(() => useKana());
+describe('when several kanas are converted to kanjis at once', () => {
+  it('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
 
-  expect(result.current.kana).toEqual('');
-
-  // Emulates to input a user's last name
-  [
-    'や',
-    'やｍ',
-    'やま',
-    'やまｄ',
-    'やまだ',
-    '山田',
-    '山田い',
-    '山田井',
-  ].forEach(value => {
-    act(() => {
-      result.current.setKanaSource({
-        value,
+    expect(result.current.kana).toEqual('');
+    ['や', 'やｍ', 'やま', 'やまｄ', 'やまだ', '山田'].forEach(value => {
+      act(() => {
+        result.current.setKanaSource({
+          value,
+        });
       });
     });
+    expect(result.current.kana).toEqual('やまだ');
   });
+});
 
-  expect(result.current.kana).toEqual('やまだい');
+describe('when kanas are converted to kanjis one by one', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['や', 'やｍ', 'やま', '山', '山ｄ', '山だ', '山田'].forEach(value => {
+      act(() => {
+        result.current.setKanaSource({
+          value,
+        });
+      });
+    });
+    expect(result.current.kana).toEqual('やまだ');
+  });
+});
+
+describe('when kanas are converted to kanjis one by one', () => {
+  // TODO Fix a bug
+  test.skip('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['や', 'やｍ', 'やま', '山', '山ｄ', '山だ', '山田', '山打'].forEach(
+      value => {
+        act(() => {
+          result.current.setKanaSource({
+            value,
+          });
+        });
+      },
+    );
+    expect(result.current.kana).toEqual('やまだ');
+  });
+});
+
+describe('when kanas are converted to katakana', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['ｋ', 'く', 'くｒ', 'くり', 'くりｓ', 'くりす', 'クリス'].forEach(
+      value => {
+        act(() => {
+          result.current.setKanaSource({
+            value,
+          });
+        });
+      },
+    );
+    expect(result.current.kana).toEqual('くりす');
+  });
+});
+
+describe('when conversion happened from head', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['ｄ', 'だ', '田', 'い田', 'いい田', '飯田'].forEach(value => {
+      act(() => {
+        result.current.setKanaSource({
+          value,
+        });
+      });
+    });
+    expect(result.current.kana).toEqual('いいだ');
+  });
+});
+
+describe('when a full-width space between characters is given', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['ｒ', 'り', '李', '李', '李　', '李　あ', '李　あい', '李　愛'].forEach(
+      value => {
+        act(() => {
+          result.current.setKanaSource({
+            value,
+          });
+        });
+      },
+    );
+    expect(result.current.kana).toEqual('り　あい');
+  });
+});
+
+describe('when a half-width space between characters is given', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    ['ｒ', 'り', '李', '李', '李 ', '李 あ', '李 あい', '李 愛'].forEach(
+      value => {
+        act(() => {
+          result.current.setKanaSource({
+            value,
+          });
+        });
+      },
+    );
+    expect(result.current.kana).toEqual('り あい');
+  });
+});
+
+describe('when a half-width space between characters is given', () => {
+  test('returns kana based on user input', () => {
+    const { result } = renderHook(() => useKana());
+
+    expect(result.current.kana).toEqual('');
+    [
+      'ｒ',
+      'り',
+      '李',
+      '李',
+      '李 ',
+      '李  ',
+      '李  あ',
+      '李  あい',
+      '李  愛',
+    ].forEach(value => {
+      act(() => {
+        result.current.setKanaSource({
+          value,
+        });
+      });
+    });
+    expect(result.current.kana).toEqual('り  あい');
+  });
 });
